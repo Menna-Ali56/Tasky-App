4519e53cbdf65ddfcc11c5c84b4a41edcfcc1db5
Menna-Ali56 <mennaalist@gmail.com>
2025-09-04 15:58:10 +0300

    - create login screen and register screen files

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tasky',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  
       home: SplashScreen(),
     );
   }
