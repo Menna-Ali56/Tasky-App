7fb87ba9116b9455606a06a33e0d0f709f7772f0
Menna-Ali56 <mennaalist@gmail.com>
2025-09-05 18:40:32 +0300

    - add  native splash screen

--- lib/feature/auth/ui/view/register_screen.dart
+++ lib/feature/auth/ui/view/register_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
-
+  static const String routeName = 'RegisterScreen';
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
